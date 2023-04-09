82f2fc33e1e809f508435f1989e27062300ceed6
CarlosEGCH <carlitosgch4@gmail.com>
2023-04-09 16:09:16 +0100

    Desktop header done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from './components/layout/Header'
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
---
+++ frontend/src/components/layout/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import "./Header.css";
+import logo from "../../assets/Logo.png";
+
+
+export default function Header() {
+  return (
+    <div className="header-wrapper">
+
+      <div className="logo-container">
+        <img src={logo} alt="Logo" />
+      </div>
+
+      <div className="links-container">
+        <div>
+          <a class="hover-underline-animation">Skills</a>
+        </div>
+        <div>
+          <a class="hover-underline-animation">Technologies</a>
+        </div>
+        <div>
+          <a class="hover-underline-animation">Projects</a>
+        </div>
+        <div>
+          <a class="hover-underline-animation">Education</a>
+        </div>
+      </div>
+
+      <div className="download-cv">
+        <a>Download CV</a>
+      </div>
+    </div>
+  )
+}
