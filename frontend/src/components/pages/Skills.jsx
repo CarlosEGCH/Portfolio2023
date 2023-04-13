3d03ae637dc54e8a77972aec23e8188391afb776
CarlosEGCH <carlitosgch4@gmail.com>
2023-04-13 11:57:25 +0100

    Skills animations done

--- frontend/src/components/pages/Skills.jsx
+++ frontend/src/components/pages/Skills.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import "./Skills.css"
 
+import { useInView, motion } from "framer-motion"
+
 export default function Skills() {
+
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    const [bars, setBars] = useState(0);
+
+    useEffect(() => {
+        if(!isInView){
+            setBars(0);
+        }
+        console.log("Bars:" + bars)
+    }, [isInView])
+
+    const frontendVar = {
+        visible: { 
+            width: "90%",
+            transition: {
+                duration: 2,
+                type: "spring",
+                stiffness: 30
+            }
+        },
+        hidden: { width: 0 },
+        }
+
+    const backendVar = {
+        visible: { 
+            width: "75%",
+            transition: {
+                delay: 0.5,
+                duration: 2,
+                type: "spring",
+                stiffness: 30
+            }
+        },
+        hidden: { width: 0 },
+        }
+    
+    const databasesVar = {
+        visible: { 
+            width: "80%",
+            transition: {
+                delay: 1,
+                duration: 2,
+                type: "spring",
+                stiffness: 30
+            }
+        },
+        hidden: { width: 0 },
+        }
+
+    const webDesignVar = {
+        visible: { 
+            width: "85%",
+            transition: {
+                delay: 1.5,
+                duration: 2,
+                type: "spring",
+                stiffness: 30
+            }
+        },
+        hidden: { width: 0 },
+        }
+
+    const uiuxVar = {
+        visible: { 
+            width: "88%",
+            transition: {
+                delay: 2,
+                duration: 2,
+                type: "spring",
+                stiffness: 30
+            }
+        },
+        hidden: { width: 0 },
+        }
+
   return (
-    <div className="skills-wrapper">
+    <div className="skills-wrapper" ref={ref}>
         <div className="left">
             <p className="title">Skills and <br/> Experience</p>
             <p className="subtitle">Junior Front-End Web Developer <span className="detail">|</span> Software Engineer <span className="detail">|</span> <br/>
@@ -63,36 +142,56 @@ HTML<span className="detail">/</span>CSS<span className="detail">/</span>Javascr
                 <div className="list-item">
                     <p>Front<span className='detail'>-</span>End</p>
                     <div className="bar-container">
-                        <div style={{width: "90%"}} className="bar-progress">
-                        </div>
+                        <motion.div
+                        variants={frontendVar}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        className="bar-progress">
+                        </motion.div>
                     </div>
                 </div>
                 <div className="list-item">
                     <p>Back<span className='detail'>-</span>End</p>
                     <div className="bar-container">
-                        <div style={{width: "70%"}} className="bar-progress">
-                        </div>
+                        <motion.div 
+                        variants={backendVar}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        className="bar-progress">
+                        </motion.div>
                     </div>
                 </div>
                 <div className="list-item">
                     <p>Databases</p>
                     <div className="bar-container">
-                        <div style={{width: "80%"}} className="bar-progress">
-                        </div>
+                        <motion.div 
+                        variants={databasesVar}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        className="bar-progress">
+                        </motion.div>
                     </div>
                 </div>
                 <div className="list-item">
                     <p>Web Design</p>
                     <div className="bar-container">
-                        <div style={{width: "85%"}} className="bar-progress">
-                        </div>
+                        <motion.div 
+                        variants={webDesignVar}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        className="bar-progress">
+                        </motion.div>
                     </div>
                 </div>
                 <div className="list-item">
                     <p>UI<span className='detail'>/</span>UX</p>
                     <div className="bar-container">
-                        <div style={{width: "88%"}} className="bar-progress">
-                        </div>
+                        <motion.div 
+                        variants={uiuxVar}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        className="bar-progress">
+                        </motion.div>
                     </div>
                 </div>
             </div>
