92381306b7e10c4c32ecfe752763d469262bc55f
CarlosEGCH <carlitosgch4@gmail.com>
2023-04-13 21:10:59 +0100

    Education section deleted for mobile momentarily

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Header from './components/layout/Header'
 import Hero from './components/pages/Hero'
@@ -10,6 +10,30 @@ import Contact from './components/pages/Contact'
 import Footer from './components/layout/Footer'
 
 export default function App() {
+
+  function useViewport() {
+    const [viewportSize, setViewportSize] = useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    useEffect(() => {
+      function handleResize() {
+        setViewportSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+
+      window.addEventListener('resize', handleResize);
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return viewportSize;
+  }
+
+  const viewport = useViewport();
+  
   return (
     <div>
       <Header />
@@ -17,9 +41,9 @@ export default function App() {
       <Skills />
       <Technologies />
       <Projects />
-      {/* <Education /> */}
-      {/* <Contact /> */}
-      {/* <Footer /> */}
+      {viewport.width > 1100 ? <Education /> : ""}
+      <Contact />
+      <Footer />
     </div>
   )
 }
